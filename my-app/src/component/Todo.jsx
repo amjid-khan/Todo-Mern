96704d618cec4fabc182056e81e477fddb43a6c8
Amjid khan <amjidkurrmywal@gmail.com>
2025-05-30 16:43:26 +0500

    add view and delete logic

--- my-app/src/component/Todo.jsx
+++ my-app/src/component/Todo.jsx
@@ -1,32 +1,58 @@
-import { useState } from 'react';
-import axios from "axios"
-import { ToastContainer, toast } from 'react-toastify';
+import { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { useEffect } from "react";
 
 function Todo() {
+  const [getTodos, setGetTodos] = useState([]);
   const [itemName, setItemName] = useState("");
   const [date, setDate] = useState("");
-  const [error , setError] = useState("")
+  const [error, setError] = useState("");
 
   const handleAdd = () => {
-  if (!itemName || !date) {
-  setError("Please fill in both fields");
-  return; // Stop here
-}
-  axios.post("http://localhost:8000/api/todos/insert", {
-    itemName,
-    date
-  })
-  .then(res => {
-    console.log("Todo added:", res.data);
-    setItemName("");
-    setDate("");
-    toast.success("Added Todo Successfully")
-  })
-  .catch(err => {
-       console.error("Error adding todo:", err.response ? err.response.data : err.message);
-  });
-}
+    if (!itemName || !date) {
+      setError("Please fill in both fields");
+      return;
+    }
+    axios
+      .post("http://localhost:8000/api/todos/insert", {
+        itemName,
+        date,
+      })
+      .then((res) => {
+        console.log("Todo added:", res.data);
+        setItemName("");
+        setDate("");
+        toast.success("Added Todo Successfully");
+        getAllTodos()
+      })
+      .catch((err) => {
+        console.error(
+          "Error adding todo:",
+          err.response ? err.response.data : err.message
+        );
+      });
+  };
 
+  const getAllTodos = async () => {
+    try {
+      const result = await axios.get("http://localhost:8000/api/todos/view");
+      setGetTodos(result.data.data);
+    } catch (err) {
+      console.log("Error fetching todos:", err);
+    }
+  };
+  useEffect(() => {
+    getAllTodos();
+  }, []);
+
+  const deleteTodos = async (id) => {
+    await axios.delete(`http://localhost:8000/api/todos/delete/${id}`)
+      .then(() => {
+        toast.success("Item Deleted Successfully")
+        getAllTodos()
+    })
+  }
 
   return (
     <div id="todo">
@@ -46,11 +72,21 @@ function Todo() {
           onChange={(e) => setDate(e.target.value)}
           type="date"
         />
-        <button onClick={handleAdd}>Add</button>
-          {error && <p style={{ color: "red" }}>{error}</p>}
-
-   
+        <button id="add" onClick={handleAdd}>Add</button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </div>
+      {getTodos.length === 0 ? <p>Add Todos To Your List</p> : <p>Your Todos List Here</p>}
+      {getTodos.map((item) => {
+        return (
+          <div id="todo-list" key={item._id}>
+            <p>{item.itemName}</p>
+            <p>{item.date}</p>
+            <div className="btns">
+              <button id="dlt-btn" onClick={()=> deleteTodos(item._id)}>Delete</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
